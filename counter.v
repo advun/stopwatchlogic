7952ae17ae84196f3a2bd2e4faa8e03d804a6811
advun <cameronpwilding@gmail.com>
2024-12-07 16:00:41 -0500

    final

--- counter.v
+++ counter.v
@@ -41,30 +41,30 @@ module counter(
     assign LNC = (~Load&Count); //Load not * Count
     
     //Q0
+    dflipflop f0(d0in, clk, reset, q[0]);
     assign d0L = (d0&Load); //d0 and Load
     assign LNCxq0 = (LNC^(q[0])); //(Load not * Count) xor q0
     assign d0in = (d0L|LNCxq0); //input to first dflipflop 
     assign carry0 = (LNC&(q[0])); //carry from the first dflipflop
-    dflipflop f0(d0in, clk, reset, q[0]);
     
     //Q1
+    dflipflop f1(d1in, clk, reset, q[1]);
     assign d1L = (d1&Load); //d1 and Load
     assign q0xq1 = (carry0^(q[1])); // carry0 xor q1
     assign d1in = (d1L|q0xq1); //input to second dflipflop 
     assign carry1 = (carry0&(q[1]));//carry from the second dflipflop
-    dflipflop f1(d1in, clk, reset, q[1]);
     
     //Q2
+    dflipflop f3(d2in, clk, reset, q[2]);
     assign d2L = (d2&Load); //d2 and Load
     assign q1xq2 = (carry1^(q[2])); // carry1 xor q2
     assign d2in = (d2L|q1xq2); //input to second dflipflop 
     assign carry2 = (carry1&(q[2]));//carry from the third dflipflop
-    dflipflop f3(d2in, clk, reset, q[2]);
     
     //Q3
+    dflipflop f4(d3in, clk, reset, q[3]);
     assign d3L = (d3&Load); //d1 and Load
     assign q2xq3 = (carry2^(q[3])); // carry2 xor q3
     assign d3in = (d3L|q2xq3); //input to second dflipflop 
-    dflipflop f4(d3in, clk, reset, q[3]);
  
 endmodule
