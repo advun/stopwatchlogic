7c0b2611564ece7fe4a760cf6635ccc7576360b8
advun <cameronpwilding@gmail.com>
2024-12-07 15:59:38 -0500

    final

--- modfivecount.v
+++ modfivecount.v
@@ -34,7 +34,7 @@ module modfivecount(
 
     counter d0(0, enable, 0, 0, 0, 0, clk, unset, q);
     assign flag = ((q[0])&(q[2])); //signal next timer if this timer is at 5
-    assign fiveout = ((q[1])&(q[2])); // 0101 = 5
-    assign unset = (fiveout|reset); //reset to 0 if 0101 or if reset
+    assign fiveout = ((q[1])&(q[2])); // 0101 = 6, so should be reset
+    assign unset = (fiveout|reset); //reset to 0 if at 6 or if reset
     
 endmodule
