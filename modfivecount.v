709ace2e3367aef3ef0d38acb1f0228948e6e633
advun <cameronpwilding@gmail.com>
2024-12-07 15:57:34 -0500

    Update modfivecount.v

--- modfivecount.v
+++ modfivecount.v
@@ -29,13 +29,12 @@ module modfivecount(
     );
     
     
-    wire fiveout, load;
+    wire fiveout, unset;
     
-    counter d0(load, enable, 0, 0, 0, 0, clk, reset, q);
-    
+
+    counter d0(0, enable, 0, 0, 0, 0, clk, unset, q);
     assign flag = ((q[0])&(q[2])); //signal next timer if this timer is at 5
-    assign fiveout = ((q[0])*(q[2])); // 0101 = 5
-    assign load = (fiveout|reset); //reset to 0 if 0101 or if reset
-    
+    assign fiveout = ((q[1])&(q[2])); // 0101 = 5
+    assign unset = (fiveout|reset); //reset to 0 if 0101 or if reset
     
 endmodule
